27936828829b60addd652f37dae1df2df249088e
David McLaren <sirusblk@gmail.com>
2015-05-06 11:47:30 -0700

    Now fixes Parcel Structure

--- fixdata.py
+++ fixdata.py
@@ -22,37 +22,42 @@ def read_in(filename):
 	output = []
 
 	with open(filename, newline='') as csv_file:
-		output = [line.split() for line in csv_file]
+		output = [line.split(',') for line in csv_file]
 
 	return output
 
 
 def find_index(property_info, text="Parcel Number"):
-	index = -1
-
 	for i, attr in enumerate(property_info[0]):
-		if attr is "Parcel Number":
+		if attr == "\"Parcel Number\"":
 			index = i
 			return index
 
-	return index
+	return -1
 
 
+# Fixes structure of Parcels to 3-3-2, some are listed as 3-2-3
 def fix_parcels(property_info):
 	index = find_index(property_info)
 
 	for line in property_info[1:]:
-		for attr in line:
-			temp = atr.strip('-')
-			
-			# DEBUG
-			print(temp)
+		attr = line[index]
+		temp = attr.replace('-','')
+		line[index] = temp[:4] + '-' + temp[4:7] + '-' + temp[7:]
 
-			line[index] = temp
+	return property_info
 
 
 def main():
-	pass
+	# Parse Arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("filename", help="Filename of .csv file to read from.")
+
+	args = parser.parse_args()
+
+	load_config()
+	csv_data = read_in(args.filename)
+	csv_data = fix_parcels(csv_data)
 
 
 if __name__ == '__main__':
